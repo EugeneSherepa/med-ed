c5faae4712ec3ac7972b5ac986a0490fd08c66cd
Eugene Sherepa <evgenijserepa@gmail.com>
2025-07-05 23:36:39 +0300

    add small fix

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from '../pages/Home/Home';
-import { About } from '../pages/About/Abour';
+import { About } from '../pages/About/About';
 
 const AppRouter = () => (
   <BrowserRouter>
