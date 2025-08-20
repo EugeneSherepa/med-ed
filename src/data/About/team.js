8126efb1b897770db9a496f9ecb83270e3376c87
Eugene Sherepa <evgenijserepa@gmail.com>
2025-08-20 13:11:46 +0300

    add home page content

--- src/data/About/team.js
+++ src/data/About/team.js
@@ -1,25 +1,88 @@
-import mentorTwoImage from '../../assets/mentors/nazrin-biochemistry.jpg';
-import mentorThreeImage from '../../assets/mentors/margo-patamarphology.jpg';
-import mentorFourImage from '../../assets/mentors/maxim-step-one-med.jpg';
+import mentorOneImage from '../../assets/mentors/inna-step-one-med.jpg';
+import mentorTwoImage from '../../assets/mentors/nika-step-one-med.jpg';
+import mentorThreeImage from '../../assets/mentors/ivanna-step-one-med.JPG';
+import mentorFourImage from '../../assets/mentors/mentor-placeholder.png';
+import mentorFiveImage from '../../assets/mentors/bohdan-step-one-med.jpg';
+import mentorSixImage from '../../assets/mentors/viktor-step-one-med.jpg';
+import mentorSevenImage from '../../assets/mentors/maxim-step-one-med.jpg';
+import mentorEightImage from '../../assets/mentors/mentor-placeholder.png';
+import mentorNineImage from '../../assets/mentors/mentor-placeholder-two.png';
+import mentorTenImage from '../../assets/mentors/mentor-placeholder-three.png';
+import mentorElevenImage from '../../assets/mentors/ivanna-step-two-med.jpg';
+import mentorTwelvImage from '../../assets/mentors/andrii-step-two-med.jpeg';
 
 const team = [
   {
     id: 2,
-    image: mentorTwoImage,
-    name: "Назрін",
-    position: "Викладач біохімії"
+    image: mentorOneImage,
+    name: "Інна",
+    position: "засновниця курсу, викладачка фармакології ЛДМУ"
   },
   {
     id: 3,
-    image: mentorThreeImage,
-    name: "Марго",
-    position: "Викладач патоморфології"
+    image: mentorTwoImage,
+    name: "Ніка",
+    position: "лікар-інтерн невролог"
   },
   {
     id: 4,
+    image: mentorThreeImage,
+    name: "Іванна",
+    position: "лікар-інтерн внутрішньої медицини"
+  },
+  {
+    id: 5,
     image: mentorFourImage,
+    name: "Катерина",
+    position: "студентка 6го курсу"
+  },
+  {
+    id: 6,
+    image: mentorFiveImage,
+    name: "Богдан",
+    position: "лікар-інтерн внутрішньої медицини"
+  },
+  {
+    id: 7,
+    image: mentorSixImage,
+    name: "Віктор",
+    position: "лікар-інтерн хірург"
+  },
+  {
+    id: 8,
+    image: mentorSevenImage,
     name: "Максим",
-    position: "Викладач КРОК-1 медицина"
+    position: "лікар-інтерн анестезіолог"
+  },
+  {
+    id: 9,
+    image: mentorEightImage,
+    name: "Олександр",
+    position: "засновник курсу, студент 4го курс стомат факультету, випускник проєкту It’s Med Ed. Результат КРОК-1 — 93%"
+  },
+  {
+    id: 10,
+    image: mentorNineImage,
+    name: "Катерина",
+    position: "засновниця курсу, студентка 4го курс стомат факультету, випускниця проєкту It’s Med Ed. Результат КРОК-1 — 91,3%"
+  },
+  {
+    id: 11,
+    image: mentorTenImage,
+    name: "Вікторія",
+    position: "студентка 4го курс стомат факультету, випускниця проєкту It’s Med Ed. Результат КРОК-1 — 88%"
+  },
+  {
+    id: 12,
+    image: mentorElevenImage,
+    name: "Іванна",
+    position: "засновниця курсу, лікар-інтерн невролог"
+  },
+  {
+    id: 13,
+    image: mentorTwelvImage,
+    name: "Андрій",
+    position: "лікар-інтерн внутрішньої медициниАртем — лікар-інтерн сімейної медицини"
   },
 ];
 
